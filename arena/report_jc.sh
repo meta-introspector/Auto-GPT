9dd3c7b747de1fef1b2423183dd6f8348862df9c
mike dupont <mike.dupont@introspector.local>
2023-10-14 18:32:24 -0400

    prep bots

--- arena/report_jc.sh
+++ arena/report_jc.sh
@@ -1,3 +1,3 @@
-jc git log --all --stat --since 2023-09-01 | jq > all_30.json
+jc git log --all --stat --since 2023-09-01 --decorate=full | jq > all_30.json
 jq -c -r  ".[]?|[.author_email,.stats.files[]?]" all.json |sort |uniq -c | sort -n > report2.txt
 jq -c -r  ".[]?|.author_email" all.json |sort |uniq -c | sort -n > report4.txt
