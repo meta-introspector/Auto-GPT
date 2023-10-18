bd79301bb4ce051837e7bc41272e8e81af761957
mike dupont <mike.dupont@introspector.local>
2023-10-18 16:58:55 -0400

    filter function

---
+++ stats/filter.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import json
+import matplotlib.pyplot as plt
+from git import Repo
+from git.cmd import GitCommandError
+
+
+repo = Repo("..")
+
+df = None
+data = []
+with open("git_samples.json") as fi:
+  for l in fi:
+      d = json.loads(l)
+      if int(d['diff']['len']) +int(d['rebase_error']['len'])> 500:
+          branches = repo.git.branch("-r","--all","--contains", d['ref'])
+          parts = branches.split("/")
+          remote = repo.git.remote("get-url",parts[1])
+          #print(parts[1],parts[2],)
+          #print(f"git submodule  add {remote} {parts[1]} && cd {parts[1]} &&git checkout {parts[1]}")
+          print(f"git submodule  add {remote} {parts[1]}")
+          #print(f"pushd {parts[1]} && git checkout {parts[1]} && popd")
--- stats/plot.py
+++ stats/plot.py
@@ -1,12 +1,30 @@
 import pandas as pd
 import json
+import matplotlib.pyplot as plt
+
 df = None
 data = []
 with open("git_samples.json") as fi:
   for l in fi:
     data.append(json.loads(l))
   df = pd.DataFrame(data)
-  fig = pd.DataFrame(sorted(df["diff"].str["len"])).plot().get_figure()
+
+  difflen    = pd.DataFrame(sorted(pd.to_numeric(df["diff"].str["len"])))
+  difflen.to_csv("difflen.csv")
+  rebase_err = pd.DataFrame(sorted(pd.to_numeric(df["rebase_error"].str["len"])))
+  rebase_err.to_csv("rebase_err.csv")
+  
+  fig = difflen.plot().get_figure()
   fig.savefig('diff_sizes.png')
-  fig = pd.DataFrame(sorted(df["rebase_error"].str["len"])).plot().get_figure()
+  fig = rebase_err.plot().get_figure()
   fig.savefig('rebase_sizes.png')
+
+  plt.hist(difflen)  
+  plt.savefig('diff_hist.png')
+  plt.hist(rebase_err)  
+  plt.savefig('rebase_hist.png')
+
+
+    
+  #fig = pd.DataFrame(sorted(df["rebase_error"].str["len"])).hist().get_figure()
+  #fig.savefig('rebase_hist.png')
